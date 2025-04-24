2722a98feb82ae67ddc70053cac609ee5d53bee5
mehrikhaoula <mehri.khawla@gmail.com>
2025-04-24 20:51:02 +0100

    auth & permission

--- routes/Admin.Routers.js
+++ routes/Admin.Routers.js
@@ -1,6 +1,5 @@
 let router=require('express').Router()
 let AdminCtrl=require('../controllers/AdminCtrl')
-const AdminModel = require('../models/AdminModel')
 
 router.post('/login', AdminCtrl.login)
 router.get('/logout',AdminCtrl.logout)
--- routes/user.Routers.js
+++ routes/user.Routers.js
@@ -1,18 +1,24 @@
 let router=require('express').Router()
 let userCtrl=require('../controllers/userCtrl')
-let auth=require('../middleware/auth')
+let Authentication= require('../middleware/Auth')
+
 //router.post('/user',userCtrl.add)
 //router.post('/division',userCtrl.Division)  // ki nesta3mlou 2 post lazem nbadlou /nom 
 //router.post('/multipl',userCtrl.Mul)
 
 router.post('/inscrit',userCtrl.inscrit)
-router.get('/users',userCtrl.allUser)
+router.get('/users', userCtrl.allUser)
 // /:id seulement dans params
 router.get('/user/:id',userCtrl.getUserById)
 router.post('/user',userCtrl.getUserId)
-router.delete('/user/:id',userCtrl.supprimUser)
+router.delete('/user/:id',Authentication.auth, (req, res) => {
+    res.json({});}, userCtrl.supprimUser)
 router.put('/user',userCtrl.updateUser)
 router.put('/updatePassword',userCtrl.updatePassword)
 
+router.use(Authentication.auth);
+router.get('/admin/settings', (req, res) => {
+    res.json({ message: 'Paramètres admin', adminId: req.admin });
+});
 
 module.exports=router
